e3e6237ac7f2a10b036087fd9cc18cd53814bacd
Emerson Ovídio <emersonovidiosantos@gmail.com>
2023-01-24 12:57:46 -0300

    start project

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    
+    public function index()
+    {
+       return Student::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Student $student
+     * @return \Illuminate\Http\Response
+     */
+
+    public function show(Student $student)
+    {
+        return $student;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::apiResource('students', \App\Http\Controllers\StudentController::class);
